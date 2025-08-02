231deff1e1c16ac7977698918d2900c0da9f5cb9
Sri Sri Gana <srisrigana369@gmail.com>
2025-08-02 10:05:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const math = require("./mathUtils");
+const math = require("./1E-1");
 
 const a = 100;
 const b = 50;
 
 console.log(`Addition of ${a} and ${b} is: ${math.add(a, b)}`);
+
 console.log(`Subtraction of ${a} and ${b} is: ${math.subtract(a, b)}`);
