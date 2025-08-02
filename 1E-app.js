ab20887aa5f207c63fbf18b000f97cd8b1692611
Sri Sri Gana <srisrigana369@gmail.com>
2025-08-02 10:07:01 +0530

    Update 1E-app.js

--- 1E-app.js
+++ 1E-app.js
@@ -1,4 +1,4 @@
-const math = require("./1E-1");
+const math = require("./1E-mathUtils");
 
 const a = 100;
 const b = 50;
@@ -6,3 +6,4 @@ const b = 50;
 console.log(`Addition of ${a} and ${b} is: ${math.add(a, b)}`);
 
 console.log(`Subtraction of ${a} and ${b} is: ${math.subtract(a, b)}`);
+
